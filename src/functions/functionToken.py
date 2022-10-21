8a422ac717c93f2870d17842d992a822f16576c9
hugoCanul <51390737+hcanul@users.noreply.github.com>
2022-10-21 12:43:25 -0500

    resuelto el problema ascii

--- src/functions/functionToken.py
+++ src/functions/functionToken.py
@@ -1,4 +1,5 @@
-from ..models import db, Token
+import unicodedata
+from ..models import db, Token, Governings, Officials
 import requests
 
 base = db.session
@@ -18,7 +19,6 @@ class fTocken:
     def getToken(self):
         response = requests.post("https://apicarrillo.felipecarrillopuerto.gob.mx/api/Keys/generate")
         data = response.json()
-        print(data)
         if data['status']:
             token = Token(
                 code = data['token'],
@@ -42,5 +42,19 @@ class fTocken:
 
     def setDbFuncionarios(self):
         response  = self.getFuncionarios()
+        data = []
         for item in response.json()['dataFuncionarios']:
-            print(str(item).encode('utf-8'))
+            data.append(
+                {
+                "id_goverming" : item["id_funcionario"],
+                "name" : unicodedata.normalize('NFKD', item["nombre_funcionario"]).encode('ASCII', 'ignore'),
+                "semblance" : unicodedata.normalize('NFKD', item["semblanza_funcionario"]).encode('ASCII', 'ignore'),
+                "url_photo" :item["foto_funcionario"],
+                "status" : item["status_funcionario"], 
+                "num_goverming" :unicodedata.normalize('NFKD', item["dependencia_funcionario"]).encode('ASCII', 'ignore'),
+                "email" : item["contacto_funcionario"],
+                }
+            )
+        print(data)
+            # print(unicodedata.normalize('NFKD', item["dependencia_funcionario"]).encode('ASCII', 'ignore'))
+
