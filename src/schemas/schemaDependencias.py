cd174c0d8ea92c8998249a3c493035353378e0ae
hcanulMpal <116234411+hcanulMpal@users.noreply.github.com>
2022-11-22 19:21:55 -0600

    ups..

--- src/schemas/schemaDependencias.py
+++ src/schemas/schemaDependencias.py
@@ -5,7 +5,7 @@ ma = Marshmallow()
 
 class schema_Dependence(ma.Schema):
     class Meta:
-        fields = ('id','dependence','schedules')
+        fields = ('id','dependence')
 
 schemaDependence = schema_Dependence()
 schemaDependences = schema_Dependence(many=True)
