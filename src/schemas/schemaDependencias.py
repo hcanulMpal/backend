efcda4fd81ea67fd32f46fdd0c21d39539f3f675
hcanulMpal <116234411+hcanulMpal@users.noreply.github.com>
2022-11-24 17:39:48 -0600

    unos pedillos

--- src/schemas/schemaDependencias.py
+++ src/schemas/schemaDependencias.py
@@ -5,7 +5,11 @@ ma = Marshmallow()
 
 class schema_Dependence(ma.Schema):
     class Meta:
+<<<<<<< Updated upstream
         fields = ('id','dependence','dia_inicial', 'dia_final', 'hora_inicial', 'hora_final','ubi_lat','ubi_long')
+=======
+        fields = ('id','dependence','dia_inicial', 'dia_final', 'hora_inicial', 'hora_final','lat','long')
+>>>>>>> Stashed changes
 
 schemaDependence = schema_Dependence()
 schemaDependences = schema_Dependence(many=True)
